443ff45d7f7f0f860cd1fd296d88ff0541b9c3aa
TryZorce <christopher.moron@gmail.com>
2024-02-22 10:23:10 +0100

    part 4 added new methods

--- player.js
+++ player.js
@@ -1,1 +1,21 @@
 console.log('player file is loaded');
+
+const player = {
+  name: "Player",
+  feedTamagotchi(tamagotchi) {
+    console.log(`Feeding ${tamagotchi.name}`);
+    tamagotchi.foodInTummy += 1;
+  },
+  medicateTamagotchi(tamagotchi) {
+    console.log(`Medicating ${tamagotchi.name}`);
+    tamagotchi.health += 1;
+  },
+  knockOutTamagotchi(tamagotchi) {
+    console.log(`Knocking out ${tamagotchi.name}`);
+    tamagotchi.restedness += 1;
+  }
+};
+
+player.feedTamagotchi(tamagotchi1);
+player.medicateTamagotchi(tamagotchi2);
+player.knockOutTamagotchi(tamagotchi1);
--- tamagotchi.js
+++ tamagotchi.js
@@ -23,12 +23,24 @@ class Tamagotchi {
     this.restedness -= 1;
     console.log(`${this.name} has current restedness of: ${this.restedness}`);
   }
+
+  start() {
+    console.log(`${this.name} has started!`);
+    this.hungerTimer = setInterval(() => this.cry(), 6000);
+    this.yawnTimer = setInterval(() => this.yawn(), 10000);
+    this.sickTimer = setInterval(() => this.puke(), 20000);
+  }
+
+  stop() {
+    console.log(`${this.name} has stopped!`);
+    clearInterval(this.hungerTimer);
+    clearInterval(this.yawnTimer);
+    clearInterval(this.sickTimer);
+  }
 }
 
 const tamagotchi1 = new Tamagotchi("Tama1", "Dragon");
 const tamagotchi2 = new Tamagotchi("Tama2", "Unicorn");
 
-tamagotchi1.cry();
-tamagotchi2.cry();
-tamagotchi1.puke();
-tamagotchi2.yawn();
+tamagotchi1.start();
+tamagotchi2.start();

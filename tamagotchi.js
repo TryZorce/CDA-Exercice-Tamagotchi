06433dff4e5b3da03700b3b08605bc6bcd840e26
TryZorce <christopher.moron@gmail.com>
2024-02-22 10:01:32 +0100

    part 1: created Tamagotchi constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,5 +1,21 @@
 console.log('tamagotchi file is loaded');
 
 class Tamagotchi {
-  // code of the class goes here
+  constructor() {
+    this.foodInTummy = 10;
+    this.restedness = 10;
+    this.health = 10;
+  }
+
+  cry() {
+    this.foodInTummy -= 1;
+    console.log("WAHH!!!");
+    console.log("Food in Tummy: " + this.foodInTummy);
+  }
 }
+
+const tamagotchi1 = new Tamagotchi();
+const tamagotchi2 = new Tamagotchi();
+
+tamagotchi1.cry();
+tamagotchi2.cry();

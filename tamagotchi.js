50b1ce47e80bba4b2d8c4e72f1c0482f02f08b2b
TryZorce <christopher.moron@gmail.com>
2024-02-22 10:11:13 +0100

    part 3: added new methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -13,13 +13,22 @@ class Tamagotchi {
     this.foodInTummy -= 1;
     console.log(`${this.name} says: WAHH!!! Food in Tummy: ${this.foodInTummy}`);
   }
+
+  puke() {
+    this.foodInTummy -= 1;
+    console.log(`${this.name} says: BLEH!!! Food in Tummy: ${this.foodInTummy}`);
+  }
+
+  yawn() {
+    this.restedness -= 1;
+    console.log(`${this.name} has current restedness of: ${this.restedness}`);
+  }
 }
 
 const tamagotchi1 = new Tamagotchi("Tama1", "Dragon");
 const tamagotchi2 = new Tamagotchi("Tama2", "Unicorn");
 
-console.log(tamagotchi1);
-console.log(tamagotchi2);
-
 tamagotchi1.cry();
 tamagotchi2.cry();
+tamagotchi1.puke();
+tamagotchi2.yawn();
